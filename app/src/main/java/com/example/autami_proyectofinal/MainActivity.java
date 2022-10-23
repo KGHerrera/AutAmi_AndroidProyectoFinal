9cc5a5fc4f16fb986582ad19703351f6eb25e43c
Enrique Herrera <bycrashdash@gmail.com>
2022-10-23 09:22:50 -0500

    se agrego la actividad menu

--- app/src/main/java/com/example/autami_proyectofinal/MainActivity.java
+++ app/src/main/java/com/example/autami_proyectofinal/MainActivity.java
@@ -2,13 +2,27 @@ package com.example.autami_proyectofinal;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button iniciarSesion;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        iniciarSesion = (Button) findViewById(R.id.btnLogin);
+
+        iniciarSesion.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, MenuActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
