053bcbb9fbcc1aea79b5ec1a3669121dc4d70ca9
Enrique Herrera <bycrashdash@gmail.com>
2022-10-23 15:17:28 -0500

    se agregaron las activities altas bajas y cambios

---
+++ app/src/main/java/com/example/autami_proyectofinal/BajasActivity.java
@@ -0,0 +1,14 @@
+package com.example.autami_proyectofinal;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class BajasActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bajas);
+    }
+}
--- app/src/main/java/com/example/autami_proyectofinal/MenuActivity.java
+++ app/src/main/java/com/example/autami_proyectofinal/MenuActivity.java
@@ -2,15 +2,53 @@ package com.example.autami_proyectofinal;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 
 public class MenuActivity extends AppCompatActivity {
+    private Button btnAltas;
+    private Button btnBajas;
+    private Button btnCambios;
+    private Button btnConsultas;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+
+        btnAltas = (Button) findViewById(R.id.btnAltas);
+        btnBajas = (Button) findViewById(R.id.btnBajas);
+        btnCambios = (Button) findViewById(R.id.btnCambios);
+        btnConsultas = (Button) findViewById(R.id.btnConsultas);
+
+        btnAltas.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MenuActivity.this, AltasActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        btnBajas.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MenuActivity.this, BajasActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        btnCambios.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MenuActivity.this, CambiosActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        
 
     }
 }
